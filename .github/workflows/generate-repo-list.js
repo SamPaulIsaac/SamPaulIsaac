691d510f014688768c7700e1779763e5967abc13
SamPaul Isaac <65417145+SamPaulIsaac@users.noreply.github.com>
2025-08-15 00:06:25 +0530

    @SamPaulIsaac: Update generate-repo-list.js

--- .github/workflows/generate-repo-list.js
+++ .github/workflows/generate-repo-list.js
@@ -1,65 +1,52 @@
 const fs = require('fs');
-const axios = require('axios');
+const { Octokit } = require('@octokit/rest');
 
-const username = process.env.GITHUB_USER;
-const token = process.env.GITHUB_TOKEN;
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN // Make sure your workflow sets GITHUB_TOKEN
+});
+
+const username = 'SamPaulIsaac';
 const readmePath = 'README.md';
 
 async function fetchRepos() {
-  const url = `https://api.github.com/user/repos?per_page=100&type=all`;
-  const res = await axios.get(url, {
-    headers: {
-      Authorization: `token ${token}`,
-      Accept: 'application/vnd.github.v3+json'
-    }
+  const repos = await octokit.paginate(octokit.repos.listForUser, {
+    username,
+    type: 'all',
+    sort: 'updated',
+    per_page: 100,
   });
-  return res.data;
+
+  return repos.map(repo => ({
+    name: repo.name,
+    description: repo.description || '',
+    stars: repo.stargazers_count,
+    forks: repo.forks_count,
+    visibility: repo.private ? 'Private' : 'Public',
+    url: repo.html_url,
+  }));
 }
 
-async function fetchCommitCount(repoName) {
-  const url = `https://api.github.com/repos/${username}/${repoName}/commits?per_page=1`;
-  const res = await axios.get(url, {
-    headers: {
-      Authorization: `token ${token}`,
-      Accept: 'application/vnd.github.v3+json'
-    }
-  });
-  const totalCommits = res.headers['link'] 
-    ? parseInt(res.headers['link'].match(/&page=(\d+)>; rel="last"/)[1]) 
-    : res.data.length;
-  return totalCommits;
-}
+function generateTable(repos) {
+  const header = '| Repository | Description | Stars | Forks | Visibility |\n| --- | --- | --- | --- | --- |\n';
+  const rows = repos.map(r =>
+    `| [${r.name}](${r.url}) | ${r.description} | ${r.stars} | ${r.forks} | ${r.visibility} |`
+  ).join('\n');
 
-async function generateMarkdown(repos) {
-  let md = '| Repository | Stars | Forks | Visibility | Commits |\n';
-  md += '|-----------|-------|-------|-----------|--------|\n';
-
-  for (const repo of repos) {
-    const commits = await fetchCommitCount(repo.name);
-    md += `| [${repo.name}](${repo.html_url}) | ${repo.stargazers_count} | ${repo.forks_count} | ${repo.private ? 'Private' : 'Public'} | ${commits} |\n`;
-  }
-
-  return md;
+  return `${header}${rows}`;
 }
 
 async function updateReadme() {
   const repos = await fetchRepos();
-  const repoMd = await generateMarkdown(repos);
+  const table = generateTable(repos);
 
-  let readme = fs.readFileSync(readmePath, 'utf8');
-  const startTag = '<!-- REPO-LIST-START -->';
-  const endTag = '<!-- REPO-LIST-END -->';
-  const regex = new RegExp(`${startTag}[\\s\\S]*${endTag}`, 'm');
+  const readme = fs.readFileSync(readmePath, 'utf8');
+  const updatedReadme = readme.replace(
+    /<!-- REPO-LIST-START -->[\s\S]*<!-- REPO-LIST-END -->/,
+    `<!-- REPO-LIST-START -->\n${table}\n<!-- REPO-LIST-END -->`
+  );
 
-  const replacement = `${startTag}\n${repoMd}\n${endTag}`;
-  if (regex.test(readme)) {
-    readme = readme.replace(regex, replacement);
-  } else {
-    readme += `\n${replacement}\n`;
-  }
-
-  fs.writeFileSync(readmePath, readme, 'utf8');
-  console.log('README.md updated with repository list including commits.');
+  fs.writeFileSync(readmePath, updatedReadme);
+  console.log('README updated successfully!');
 }
 
 updateReadme().catch(err => {
